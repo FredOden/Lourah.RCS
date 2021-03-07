ffa4930e83dd59d70b16ad1ffb178d8c8b0aeb14
Fred Oden <fred.oden@gmail.com>
2021-03-07 17:15:38 +0100

    Create::Lourah.RCS.Middleware.js,Lourah.RCS.Repository::Initial source files

---
+++ Lourah.RCS.Middleware.js
@@ -0,0 +1,18 @@
+/**
+ * Interface Lourah.RCS.Middleware
+ * Is the model for all middleware implementations
+ * 
+ * A middleware is in charge to manage the repository through
+ * a transport layer
+ * 
+ */
+
+var Lourah = Lourah || {};
+(function (){
+	Lourah.RCS = Lourah.RCS || {};
+	if (Lourah.RCS.Middleware) return;
+	function Middleware() {
+	}
+	Lourah.RCS.Middleware = Middleware
+})();
+
---
+++ Lourah.RCS.Repository.js
@@ -0,0 +1,16 @@
+/**
+ * Interface Lourah.RCS.Repository
+ * 
+ * Physical content of repository with historical information
+ * 
+ */
+
+var Lourah = Lourah || {};
+(function (){
+	Lourah.RCS = Lourah.RCS || {};
+	if (Lourah.RCS.Repository) return;
+	function Repository() {
+	}
+	Lourah.RCS.Repository = Repository
+})();
+
